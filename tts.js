51e1313fcf276446d0ba226d3c861ac4360c57cd
guillain <guillain@gmail.com>
2019-05-01 23:26:29 +0200

    under class

--- tts.js
+++ tts.js
@@ -1,23 +1,33 @@
 // Require the module
 var picoSpeaker = require('pico-speaker');
- 
-// Define configuration
-/*
-var picoConfig = {
-   AUDIO_DEVICE: 'default:CARD=PCH',
-   LANGUAGE: 'fr-FR'
-};*/
-/* DEFAULT CONFIG */
-var picoConfig = {
-   AUDIO_DEVICE: null, // will use default alsa device
-   LANGUAGE: 'en-US'
-};
 
-// Initialize with config
-picoSpeaker.init(picoConfig);
- 
-// Say hello
-picoSpeaker.speak('Hello !').then(function() {
-       // console.log("done");
-    }.bind(this))
+// lang='fr-FR';
+// device='CARD=PCH';
+
+
+// Define the TTS class
+class TTS {
+    constructor(lang='en-US', device=null){
+        this.config = {
+            AUDIO_DEVICE: lang, // will use default alsa device
+            LANGUAGE: device
+        };
+    }
+
+    // Initialize with config
+    init(config=''){
+        config = (config != '') ? config : this.config;
+        picoSpeaker.init(config);
+    }
+
+    say(data='hello !'){
+        picoSpeaker.speak(data).then(function() {
+            console.log("say data:%s", data);
+        }.bind(this))
+    }
+}
+
+tts = new TTS(lang='fr-FR');
+tts.init();
+tts.say('salut les terriens');
 
